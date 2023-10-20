bb1d253b8da2a3f27f990b3f7f9ae2aef69c1a2a
ival06001 <ival06001@E-PW024NVZ>
2023-10-20 10:11:39 +0200

    Add project files.

---
+++ Uppgift 4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =10 ; i <= 30; i++)
+            {
+                 Console.WriteLine(i);
+            }
+
+            for (int j = 200; j >= 180; j--)
+            {
+                 Console.WriteLine(j);
+            }
+
+             for ( int g = 1000; g <= 1400; g+=50)
+             {
+                Console.WriteLine(g);
+             }
+
+        }
+    }
+}
